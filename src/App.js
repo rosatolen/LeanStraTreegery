2762a509adc610e90e564a710daa981bd55d101d
Coleman Beasley <cbeasley@thoughtworks.com>
2017-12-05 15:28:42 -0800

    Clean up code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
-import logo from './logo.svg';
 import './App.css';
 import TreeVis from './TreeVis';
 import AddNodeModal from './AddNodeModal';
