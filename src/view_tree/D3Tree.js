795166b667954d3a9c609978fa276c0cdcff29c7
Coleman Beasley <cbeasley@thoughtworks.com>
2018-01-18 11:36:09 -0800

    update connecting lines to use bezier curves

--- src/view_tree/D3Tree.js
+++ src/view_tree/D3Tree.js
@@ -9,12 +9,10 @@ class D3Tree extends Component {
   constructor(props) {
     super(props);
 
-    this.horizontalSize = props.width;
-    this.verticalSize = props.height;
     this.horizontalPadding = 10;
     this.verticalPadding = 10;
-    this.width = this.horizontalSize - 2 * this.horizontalPadding;
-    this.height = this.verticalSize - 2 * this.verticalPadding;
+    this.width = props.width - 2 * this.horizontalPadding;
+    this.height = props.height - 2 * this.verticalPadding;
     this.nodeHeight = 120;
     this.nodeWidth = 180;
 
@@ -34,8 +32,13 @@ class D3Tree extends Component {
     return links.map(link => {
       let source = link.source;
       let target = link.target;
-      let key = `${source.data.name} -> ${target.data.name}`;
-      return (<line key={key} x1={source.x + 75} y1={source.y + 75} x2={target.x} y2={target.y} stroke="black" />);
+      let key = `${source.data.name} -> ${target.data.name}`
+      let linkGenerator = d3.linkVertical()
+        .x((d) => { return d.x; })
+        .y((d) => { return d.y; });
+      // let line = (<line key={key} x1={source.x + 75} y1={source.y + 75} x2={target.x} y2={target.y} stroke="black" />);
+      let path = (<path key={key} d={linkGenerator(link)} fill="none" stroke="black" />);
+      return path;
     });
   }
 
@@ -67,8 +70,8 @@ class D3Tree extends Component {
         {/* <svg viewBox={`0 0 ${this.props.width - 2 * this.horizontalPadding} ${this.props.height - 2 * this.verticalPadding}`}> */}
         <svg width={this.props.width} height={this.props.height} ref={this.addZoomTransform}>
           <g id="svgContents">
-          {links}
-          {nodes}
+            {links}
+            {nodes}
           </g>
         </svg>
       </div>
