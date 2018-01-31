f325365797fbb1b4e6f2c271f49bf8370bbae55c
Coleman Beasley <cbeasley@thoughtworks.com>
2018-01-31 10:28:19 -0800

    move nodeData to state, and update on next props

--- src/view_tree/TreeNode.js
+++ src/view_tree/TreeNode.js
@@ -5,9 +5,9 @@ import '../App.css';
 class TreeNode extends Component {
   constructor(props) {
     super(props);
-    this.nodeData = props.data;
     this.state = {
-      truncate: false
+      truncate: false,
+      nodeData: props.data
     }
   }
 
@@ -17,6 +17,14 @@ class TreeNode extends Component {
     this.setState({
       truncate: shouldTruncate
     });
+  }
+
+  componentWillReceiveProps = (nextProps) => {
+    if(nextProps.data !== this.state.nodeData) {
+      this.setState({
+        nodeData: nextProps.data
+      });
+    }
   }
 
   render = () => {
@@ -39,9 +47,9 @@ class TreeNode extends Component {
       <svg
         width={this.props.width}
         height={this.props.height}
-        x={this.nodeData.x - this.props.width / 2}
-        y={this.nodeData.y}
-        onDoubleClick={this.props.doubleClickListener.bind(this, this.nodeData.data)}
+        x={this.state.nodeData.x - this.props.width / 2}
+        y={this.state.nodeData.y}
+        onDoubleClick={this.props.doubleClickListener.bind(this, this.state.nodeData.data)}
         className={'tree_node'} >
         <rect
           width={this.props.width}
@@ -60,7 +68,7 @@ class TreeNode extends Component {
             y='1em'
             className={'title'}
           >
-            {this.nodeData.data.title}
+            {this.state.nodeData.data.title}
           </text>
           <foreignObject
             width={this.props.width}
@@ -70,8 +78,7 @@ class TreeNode extends Component {
           >
             <div className='svg_description'>
               <div ref={this.checkForTruncate}>
-                {this.nodeData.data.description}
-                something short
+                {this.state.nodeData.data.description}
             </div>
             </div>
           </foreignObject>
