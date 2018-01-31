2b625bcd096b90b29593f6870b75c5707cf30d14
Coleman Beasley <cbeasley@thoughtworks.com>
2018-01-31 11:45:56 -0800

    Fade in ellipse svg's background

--- src/view_tree/TreeNode.js
+++ src/view_tree/TreeNode.js
@@ -20,7 +20,7 @@ class TreeNode extends Component {
   }
 
   componentWillReceiveProps = (nextProps) => {
-    if(nextProps.data !== this.state.nodeData) {
+    if (nextProps.data !== this.state.nodeData) {
       this.setState({
         nodeData: nextProps.data
       });
@@ -30,60 +30,42 @@ class TreeNode extends Component {
   render = () => {
     let ellipseSvg = (
       <svg
-        width={'3em'}
-        height={'1em'}
-        x={175}
-        y={'3.5em'}
-        className='svg_ellipse'
-      >
-        <text
-          y={'.8em'}
-        >
+        width={'2em'} height={'1em'}
+        x={175} y={'3.5em'} >
+        <defs>
+          <linearGradient id='white_fade' >
+            <stop offset='0%' stopColor='white' stopOpacity='0' />
+            <stop offset='5%' stopColor='white' stopOpacity='.5' />
+            <stop offset='40%' stopColor='white' stopOpacity='1' />
+          </linearGradient>
+        </defs>
+        <rect width='100%' height='100%' fill={'url(#white_fade)'} stroke='none' />
+        <text y={'.8em'} x={'.7em'}>
           ...
         </text>
       </svg>
     );
     return (
-      <svg
-        width={this.props.width}
-        height={this.props.height}
-        x={this.state.nodeData.x - this.props.width / 2}
-        y={this.state.nodeData.y}
+      <svg width={this.props.width} height={this.props.height}
+        x={this.state.nodeData.x - this.props.width / 2} y={this.state.nodeData.y}
         onDoubleClick={this.props.doubleClickListener.bind(this, this.state.nodeData.data)}
-        className={'tree_node'} >
-        <rect
-          width={this.props.width}
-          height={this.props.height}
-          x={0}
-          y={0}
-        />
-        <svg
-          width={this.props.width}
-          height={this.props.height}
-          x={0}
-          y={0}
-        >
-          <text
-            height='1em'
-            y='1em'
-            className={'title'}
-          >
+        className={'tree_node'}
+      >
+        <rect width={this.props.width} height={this.props.height} x={0} y={0} className='background_rect' />
+        <svg width={this.props.width} height={this.props.height} x={0} y={0} >
+          <text height='1em' y='1em' className={'title'} >
             {this.state.nodeData.data.title}
           </text>
-          <foreignObject
-            width={this.props.width}
-            height={'3em'}
-            // height='auto'
-            y={'1.5em'}
-          >
+          <foreignObject width={this.props.width} height={'3em'} y={'1.5em'} >
             <div className='svg_description'>
               <div ref={this.checkForTruncate}>
                 {this.state.nodeData.data.description}
-            </div>
+              </div>
             </div>
           </foreignObject>
           {this.state.truncate ? ellipseSvg : null}
         </svg>
+        <rect width={this.props.width} height={this.props.height} x={0} y={0} className='bounding_rect' />
       </svg>
     );
   }
